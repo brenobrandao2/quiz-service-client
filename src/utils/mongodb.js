f7158e430113688402b0401bc77f4cf385673344
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-08 00:10:18 -0300

    fix: Altera url mongodb

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export default class MongoDb {
     constructor() {
-        this.uri = 'mongodb://lam_quiz:HndC$1087Lam%402020a@159.203.187.163/life_and_money_quiz'
-        this.options =  { useNewUrlParser: true }
+        this.uri = 'mongodb://localhost/life_and_money_quiz'
+        this.options =  {user: 'lam_quiz', pass: 'HndC$1087Lam@2020a', useNewUrlParser: true }
         this.mongo = new mongoose.Mongoose(({ uri: this.uri, options: this.options}))
     }
 
